39884919ea13584a4509db4f5aa4af4b50442766
a-klimovich <kom9toshka@gmail.com>
2023-04-01 20:38:51 +0200

    modal

--- src/js/main.js
+++ src/js/main.js
@@ -9,5 +9,32 @@ if (userIcon) {
 }
 
 (function () {
-  console.log("done");
+  const openModalBtn = document.querySelectorAll(".open-modal-btn");
+  const modal = document.querySelector(".modal");
+  
+  openModalBtn.forEach((item) => {
+    item.addEventListener("click", () => {
+      modal.style.display = "flex";
+    });
+  })
+
+  const closeBtn = document.querySelector(".close");
+
+  closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+
+  window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+
+  const form = document.querySelector("form");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // код для отправки данных формы на сервер
+    modal.style.display = "none";
+  });
 })();
