462fbf796309c71462a37aa337f561f320cfb474
a-klimovich <kom9toshka@gmail.com>
2023-04-02 14:03:49 +0200

    header mobile menu fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-// EXEMPLE lib Vivus
+// Vivus library example
 const userIcon = document.getElementById("user");
 
 if (userIcon) {
@@ -8,18 +8,20 @@ if (userIcon) {
   });
 }
 
+// Modal functionality
 (function () {
+  // Get required elements
   const openModalBtn = document.querySelectorAll(".open-modal-btn");
   const modal = document.querySelector(".modal");
-  const header = document.querySelector(".header");
+  const closeBtn = document.querySelector(".close");
+  const form = document.querySelector("form");
 
+  // Add event listeners
   openModalBtn.forEach((item) => {
     item.addEventListener("click", () => {
       modal.style.display = "flex";
     });
   });
-
-  const closeBtn = document.querySelector(".close");
 
   closeBtn.addEventListener("click", () => {
     modal.style.display = "none";
@@ -31,32 +33,55 @@ if (userIcon) {
     }
   });
 
-  const form = document.querySelector("form");
-
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    // код для отправки данных формы на сервер
+    // Code to send form data to the server
     modal.style.display = "none";
   });
+})();
 
+// Mobile menu functionality
+(function () {
+  // Get required elements
+  const header = document.querySelector('.header');
   const showPhoneBtn = document.querySelector("#show-phone-btn");
   const openMobileMenuBtn = document.querySelector("#open-mobile-menu-btn");
   const mobileMenuHeadTitle = document.querySelector(".mobile-menu-head .title");
+  const mobileMenuCloseBtn = document.querySelector("#mobile-menu-closer");
+  const buttons = document.querySelectorAll(".open-submenu-btn");
 
+  const handleClearHeaderClass = (className) => {
+    if (header.classList.contains(className)) {
+      header.classList = "header";
+    }
+  }
+
+  // Add event listeners
   showPhoneBtn.addEventListener("click", () => {
+    handleClearHeaderClass("show-menu")
     header.classList.toggle("show-phone");
     mobileMenuHeadTitle.innerHTML = "ТЕЛЕФОН";
   });
 
   openMobileMenuBtn.addEventListener("click", () => {
+    handleClearHeaderClass("show-phone")
     header.classList.toggle("show-menu");
     mobileMenuHeadTitle.innerHTML = "МЕНЮ";
   });
-
-  const mobileMenuCloseBtn = document.querySelector("#mobile-menu-closer");
 
   mobileMenuCloseBtn.addEventListener("click", () => {
     header.classList = "header";
   });
 
+  buttons.forEach((button) => {
+    const parent = button.parentNode;
+    const submenu = parent.querySelector("ul");
+    if (!submenu) {
+      button.style.display = "none";
+    }
+
+    button.addEventListener("click", () => {
+      parent.classList.toggle("active");
+    });
+  });
 })();
