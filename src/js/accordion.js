dad5dc4bed8a833bdacfc30f5d962246fc183187
a-klimovich <kom9toshka@gmail.com>
2023-04-02 18:04:59 +0200

    add media + section form

---
+++ src/js/accordion.js
@@ -0,0 +1,13 @@
+(function () {
+  const accordionItems = document.querySelectorAll(".accordion-item");
+
+  accordionItems.forEach((item) => {
+    const header = item.querySelector(".accordion-header");
+    const content = item.querySelector(".accordion-content");
+
+    header.addEventListener("click", () => {
+      item.classList.toggle("active");
+      content.classList.toggle("active");
+    });
+  });
+})();
