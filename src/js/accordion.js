204eddca4d506621f1612d55e6ec9ed2c19c1821
a-klimovich <kom9toshka@gmail.com>
2023-04-02 19:25:02 +0200

    accardion section

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -3,11 +3,9 @@
 
   accordionItems.forEach((item) => {
     const header = item.querySelector(".accordion-header");
-    const content = item.querySelector(".accordion-content");
 
     header.addEventListener("click", () => {
       item.classList.toggle("active");
-      content.classList.toggle("active");
     });
   });
 })();
