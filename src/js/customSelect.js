ada9e940cdb2fa2df2f3424f255e9077ebf8836b
a-klimovich <kom9toshka@gmail.com>
2023-04-03 01:17:19 +0200

    custom input select

---
+++ src/js/customSelect.js
@@ -0,0 +1,33 @@
+(() => {
+  $(".select").each(function () {
+    const $select = $(this);
+    const $head = $select.find(".select__head");
+
+    $(".decore").closest(".select__head").addClass("has-decore");
+
+    $head.on("click", function () {
+      const $list = $(this).parent().find(".select__list");
+      $(this).toggleClass("open");
+      $list.toggleClass("open");
+    });
+
+    $select.on("click", ".select__item", function (e) {
+      e.stopPropagation();
+      const value = $(this).attr("data-value");
+      const text = $(this).text();
+      const $list = $(this).parent();
+      const $input = $select.find(".select__input");
+      const $value = $select.find(".chousen-value");
+      $value.text(text);
+      $list.removeClass("open");
+      $head.removeClass("open");
+      $input.val(value).trigger("change");
+    });
+
+    $(document).on("click", function (e) {
+      if (!$select.has(e.target).length) {
+        $select.find(".select__list").removeClass("open");
+      }
+    });
+  });
+})();
