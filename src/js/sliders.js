018ff4b630dbbeb2bf4dc0dae7731c25ab8720c9
a-klimovich <kom9toshka@gmail.com>
2023-04-03 04:38:59 +0200

    catalogs

--- src/js/sliders.js
+++ src/js/sliders.js
@@ -23,5 +23,56 @@ $(document).ready(function(){
       }
     ]
   });
+
+  
+  const slidesInRow = () => {
+    const countSlides = $('#brand-logo-slider .brand-logo-slide')?.length;
+    const screenSize = $(window)[0].innerWidth;
+    const rowOnMobile = 3;
+
+    if (screenSize <= 768) {
+      return rowOnMobile;
+    }
+    if (screenSize <= 1200) {
+      return countSlides / 4;
+    }
+    if (screenSize >= 1200) {
+      console.log(21);
+      return countSlides / 6;
+    }
+  };
+
+  $('#brand-logo-slider').slick({
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    arrows: false,
+    infinite: false,
+    rows: slidesInRow(),
+    appendDots: $('#brand-logo-slider'),
+    customPaging: function(slider, i) {
+      return '<button class="slick-progress" role="button" data-slide="' + i + '"></button>';
+    },
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          dots: true,
+        }
+      }
+    ]
+  })
+  $('.slick-progress').click(function() {
+    var index = $(this).data('slide');
+    $('.slider').slick('slickGoTo', index);
+  });
 });
 	
