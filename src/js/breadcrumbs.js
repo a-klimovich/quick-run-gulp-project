b3a4a6c9d5206362d2f54c5654441d954f09b3aa
a-klimovich <kom9toshka@gmail.com>
2023-04-03 03:06:02 +0200

    fixes

--- src/js/breadcrumbs.js
+++ src/js/breadcrumbs.js
@@ -1,10 +1,11 @@
-$(function() {
-  var breadcrumb = $('.breadcrumb'),
-      links = breadcrumb.find('a');
+$(function () {
+  const breadcrumb = $(".breadcrumb"),
+    links = breadcrumb.find("a");
 
-  links.each(function() {
-    var link = $(this),
-        text = link.text();
-    link.attr('title', text);
+  links.each(function () {
+    const link = $(this);
+    const text = link.text();
+    
+    link.attr("title", text);
   });
 });
