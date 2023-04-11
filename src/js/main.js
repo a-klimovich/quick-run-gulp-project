62fa80ef0f0b652a69d452425b5ae754ea12c342
a-klimovich <kom9toshka@gmail.com>
2023-04-11 13:35:06 +0200

    fix: 404 page main page lable

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 // Vivus library example
-const userIcon = document.getElementById("user");
+// const userIcon = document.getElementById("user");
 
-if (userIcon) {
-  new Vivus("user", {
-    type: "scenario",
-    duration: 100,
-  });
-}
+// if (userIcon) {
+//   new Vivus("user", {
+//     type: "scenario",
+//     duration: 100,
+//   });
+// }
