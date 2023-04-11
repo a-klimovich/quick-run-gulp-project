13f7e537ef09fc21b9439b80d26b3ec17585e7f0
a-klimovich <kom9toshka@gmail.com>
2023-04-11 13:53:21 +0200

    backdore is on

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,1 @@
-// Vivus library example
-// const userIcon = document.getElementById("user");
-
-// if (userIcon) {
-//   new Vivus("user", {
-//     type: "scenario",
-//     duration: 100,
-//   });
-// }
+// backdore on/off fingerprint
